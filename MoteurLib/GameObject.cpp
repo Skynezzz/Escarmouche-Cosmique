608eb42ddf303dbeb9cc45897b1bb72a7b48fe35
kindhee <rmichel@gaming.tech>
2024-02-27 11:34:24 +0100

    transform

--- MoteurLib/GameObject.cpp
+++ MoteurLib/GameObject.cpp
@@ -1,58 +1,11 @@
 #include "pch.h"
 #include "GameObject.h"
 
-void GameObject::Identity()
-{
-}
-
-void GameObject::FromMatrix(MATRIX* pMat)
-{
-}
-
-void GameObject::UpdateRotationFromVectors()
-{
-}
-
-void GameObject::UpdateRotationFromQuaternion()
-{
-}
-
-void GameObject::UpdateRotationFromMatrix()
-{
-}
-
-void GameObject::UpdateMatrix()
-{
-}
 
 void GameObject::Rotate(float yaw, float pitch, float roll)
 {
-}
+	XMVECTOR XM_CALLCONV XMLoadFloat3(const XMFLOAT3 * vSca);
 
-void GameObject::RotateYaw(float angle)
-{
-}
+	void XM_CALLCONV XMStoreFloat3(XMFLOAT3 * vSca, FXMVECTOR V);
 
-void GameObject::RotatePitch(float angle)
-{
-}
-
-void GameObject::RotateRoll(float angle)
-{
-}
-
-void GameObject::RotateWorld(MATRIX* pMatrix)
-{
-}
-
-void GameObject::RotateWorldX(float angle)
-{
-}
-
-void GameObject::RotateWorldY(float angle)
-{
-}
-
-void GameObject::RotateWorldZ(float angle)
-{
 }
--- MoteurLib/GameObject.h
+++ MoteurLib/GameObject.h
@@ -11,37 +11,9 @@ public:
 
 private :
 
-	void Identity();
-	void FromMatrix(MATRIX* pMat);
-	void UpdateRotationFromVectors();
-	void UpdateRotationFromQuaternion();
-	void UpdateRotationFromMatrix();
-	void UpdateMatrix();
-
 	void Rotate(float yaw, float pitch, float roll);
-	void RotateYaw(float angle);
-	void RotatePitch(float angle);
-	void RotateRoll(float angle);
-	void RotateWorld(MATRIX* pMatrix);
-	void RotateWorldX(float angle);
-	void RotateWorldY(float angle);
-	void RotateWorldZ(float angle);
 
 };
 
-struct TRANSFORM {
-	VECTOR3 vSca;
-	MATRIX mSca;
 
-	VECTOR3 vDir;
-	VECTOR3 vRight;
-	VECTOR3 vUp;
-	QUATERNION qRot;
-	MATRIX mRot;
-
-	VECTOR3 vPos;
-	MATRIX mRot;
-
-	MATRIX matrix;
-};
 
