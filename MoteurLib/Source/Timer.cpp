9fbf62b0c460c413950ee18dba7a05ef558c4190
Guilherme Coelho Vieira <gui130504.gv@gmail.com>
2024-02-28 17:23:02 +0100

    Timer.h / Timer.cpp

--- MoteurLib/Source/Timer.cpp
+++ MoteurLib/Source/Timer.cpp
@@ -1,1 +1,75 @@
+#include <windows.h>
 #include "Timer.h"
+
+Timer::Timer() :
+    mSecondsPerCount(0.0), mDeltaTime(-1.0), mBaseTime(0), mStopTimePressed(0),
+    mStopTime(0), mPreviousTime(0), mCurrentTime(0), mIsStopped(false) 
+{
+    __int64 countsPerSec;
+    QueryPerformanceFrequency((LARGE_INTEGER*)&countsPerSec);
+    mSecondsPerCount = 1.0 / static_cast<double>(countsPerSec);
+}
+
+float Timer::TotalTime() {
+    if (mIsStopped == true) {
+        return static_cast<float>(((mStopTime - mStopTimePressed) - mBaseTime) * mSecondsPerCount);
+    }
+    else {
+        return static_cast<float>(((mCurrentTime - mStopTime) - mBaseTime) * mSecondsPerCount);
+    }
+}
+
+float Timer::DeltaTime() {
+    return static_cast<float>(mDeltaTime);
+}
+
+void Timer::Start() {
+    __int64 startTime;
+    QueryPerformanceCounter((LARGE_INTEGER*)&startTime);
+    if (mIsStopped == true) {
+        mStopTime += (startTime - mStopTimePressed);
+        mPreviousTime = startTime;
+        mStopTimePressed = 0;
+        mIsStopped = false;
+    }
+}
+
+void Timer::Stop() {
+    if (mIsStopped == false) {
+        __int64 stopTime;
+        QueryPerformanceCounter((LARGE_INTEGER*)&stopTime);
+
+        mStopTimePressed = stopTime;
+        mIsStopped = true;
+    }
+}
+
+void Timer::Reset() {
+    __int64 currentTime;
+    QueryPerformanceCounter((LARGE_INTEGER*)&currentTime);
+
+    mBaseTime = currentTime;
+    mPreviousTime = currentTime;
+    mStopTime = 0;
+    mStopTimePressed = 0;
+    mIsStopped = false;
+}
+
+void Timer::Tick() {
+
+    if (mIsStopped == true) {
+        mDeltaTime = 0.0;
+        return;
+    }
+
+    __int64 currentTime;
+    QueryPerformanceCounter((LARGE_INTEGER*)&currentTime);
+    mCurrentTime = currentTime; 
+
+    mDeltaTime = ((mCurrentTime - mPreviousTime) * mSecondsPerCount);
+
+    if (mDeltaTime > 0) {
+        mDeltaTime = 0;
+    }
+    mPreviousTime = mCurrentTime;
+}
--- MoteurLib/Source/Timer.h
+++ MoteurLib/Source/Timer.h
@@ -1,5 +1,26 @@
 #pragma once
 class Timer
 {
+public:
+	Timer();
+	float TotalTime(); 
+	float DeltaTime();
+
+	void Start(); //Sert à lancer le timer
+	void Stop(); //Sert à mettre en pause le timer
+	void Reset(); //Sert à réinitialiser le timer
+	void Tick();
+
+private:
+	float mSecondsPerCount;
+	float mDeltaTime;
+
+	float mBaseTime;
+	float mStopTimePressed; //Sert à récupérer le temps où le timer à été mis en pause (Sert ensuite de point de départ lors du resume)
+	float mStopTime; //Sert à contenir tout le temps écoulé lors de l'état "EN PAUSE" 
+	float mPreviousTime;
+	float mCurrentTime; //Sert à contenir le temps actuel
+
+	bool mIsStopped; //Sert d'état pour savoir si oui ou non le temps est en pause
 };
 
